f8352d6be484ce45cd2f364e22b987d908dc4918
Ruben Aguilar <ruben.aguilar@itrends.mx>
2024-03-12 00:41:39 -0600

    Conexion con la base de datos

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,16 @@
 import 'package:recordatorios_app/screen/screens.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() => runApp(const MyApp());
+
+//void main() => runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MyApp());
+}
 
 
 
